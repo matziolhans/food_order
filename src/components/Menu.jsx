7e848fd8e53a8acd531187804380fa94cd8347d8
Hans <hans.matziol@stud.uni-goettinge.de>
2023-12-06 13:16:05 +0100

    add new Meals

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,6 +6,9 @@ function Menu() {
   const [meals, seatMenu] = useState([
     { id: "1", name: "Steak", price: "10", image: "src/assets/Steak.jpg" },
     { id: "2", name: "Burger", price: "8", image: "src/assets/Burger.jpg" },
+    { id: "3", name: "Pizza", price: "15", image: "src/assets/Pizza.jpg" },
+    { id: "4", name: "Salad", price: "5", image: "src/assets/Salad.jpg" },
+    { id: "5", name: "Pasta", price: "10", image: "src/assets/Pasta.jpg" },
   ]);
 
   return (
