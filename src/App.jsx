48c74906008ae31c1e19d75d0c48eda2aa51c1b9
Hans <hans.matziol@stud.uni-goettinge.de>
2023-12-07 14:33:08 +0100

    add Cart Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Hero from "./components/Hero";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
+import { CartProvider } from "./store/CartContext";
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <Header />
       <Hero />
       <Menu />
-    </>
+    </CartProvider>
   );
 }
 
