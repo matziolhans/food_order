a91b3cace7fa70f2c45fe81c59b4aaadd3d4647c
Hans <hans.matziol@stud.uni-goettinge.de>
2023-12-08 15:43:55 +0100

    finish Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,26 +2,56 @@ import { useContext } from "react";
 import Modal from "./Modal";
 import CartContext from "../store/CartContext";
 import UserProgressContext from "../store/UserProgressContext";
+import classes from "./Cart.module.css";
 
 function Cart() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
 
   const price = cartCtx.items.reduce((totalPrice, item) => {
-    totalPrice + item.quantity * item.price;
+    return totalPrice + item.quantity * item.price;
   }, 0);
 
+  const shouldRenderModal = userProgressCtx.userProgress === "cart";
+
+  if (!shouldRenderModal) {
+    return null;
+  }
+
+  function handleClose() {
+    userProgressCtx.hideCart();
+  }
+ 
   return (
-    <Modal open={userProgressCtx.userProgress === "cart"}>
+    <Modal
+      open={userProgressCtx.userProgress === "cart"}
+      className={classes.modal}
+    >
       <h2>Cart</h2>
-      <ul>
-        {cartCtx.items.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-      <p>{price}€</p>
-      <button>Close</button>
-      <button>Checkout</button>
+      <div className={classes.order}>
+        <ul>
+          {cartCtx.items.map((item) => (
+            <li key={item.id}>
+              <img src={item.image} alt={item.name} />
+              <p>
+                {item.name} - {item.price}€
+              </p>
+              <div className={classes.quantitySelection}>
+                <button onClick={() => cartCtx.addItem(item)}>+</button>
+                <p>{item.quantity} </p>
+                <button onClick={() => cartCtx.removeItem(item.id)}>-</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={classes.footer}>
+        <div className={classes.navigation}>
+          <button onClick={handleClose}>Close</button>
+          {cartCtx.items.length > 0 && <button>Checkout</button>}
+        </div>
+        <p>Price: {price}€</p>
+      </div>
     </Modal>
   );
 }
